64a96dcf2f06fa8837acc75069b9ad6622c536e7
salem21 <mrsalem_ray@hotmail.com>
2014-11-05 11:15:30 -0600

    05-tempra

---
+++ sites/all/modules/extenso/extenso-block-view.tpl.php
@@ -0,0 +1,15 @@
+<p id="extenso"><em>
+	<?php
+	echo "El articulo mas grande es: <br>";
+
+	echo "<div id=mas_grande> ";
+	echo $items[0]['data'];
+
+	echo "</div>";
+
+	echo "¿Te atreves a leerlo?";
+	
+
+	  ?>
+
+</em></p>
